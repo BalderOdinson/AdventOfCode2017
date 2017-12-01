e14f64141a87b7801df067d05d387aff32581a1c
Dorian Brmbota <dorianb2@hotmail.com>
2017-12-01 18:00:46 +0100

    Add project files.

---
+++ FirstPuzzle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+               throw new ArgumentNullException(nameof(args));
+            Console.WriteLine($"Rezultat prve polovice je: {new PuzzleSolver(args[0]?.Select(c => Int32.Parse(c.ToString()))).SolveFirstHalf()}");
+            Console.WriteLine($"Rezultat druge polovice je: {new PuzzleSolver(args[0]?.Select(c => Int32.Parse(c.ToString()))).SolveSecondHalf()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FirstPuzzle/PuzzleSolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPuzzle
+{
+    public class PuzzleSolver
+    {
+        private readonly List<int> _digits;
+
+        public PuzzleSolver(IEnumerable<int> digits)
+        {
+            _digits = digits.ToList();
+        }
+
+        public int SolveFirstHalf()
+        {
+            var previousDigit = _digits[_digits.Count - 1];
+            var sum = 0;
+            foreach (var digit in _digits)
+            {
+                if (digit == previousDigit)
+                    sum += digit;
+                previousDigit = digit;
+            }
+            return sum;
+        }
+
+        public int SolveSecondHalf()
+        {
+            var sum = 0;
+            var count = _digits.Count;
+            var halfway = count / 2;
+            for (int i = 0; i < count; i++)
+            {
+                if (_digits[(halfway + i) % count] == _digits[i])
+                    sum += _digits[i];
+            }
+            return sum;
+        }
+
+    }
+}
