35dc616e6ad2f9c5c3602b7d86a93a78d4c100c6
Dorian Brmbota <dorianb2@hotmail.com>
2017-12-02 23:50:45 +0100

    Solved second puzzle.

---
+++ SecondPuzzle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SecondPuzzle
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select a input file";
+            openFileDialog.Filter = "Txt file|*.txt";
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+            var input = File.ReadAllLines(openFileDialog.FileName);
+            var puzzleSolver = new PuzzleSolver(input);
+            Console.WriteLine(puzzleSolver.SolveFirst());
+            Console.WriteLine(puzzleSolver.SolveSecond());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SecondPuzzle/PuzzleSolver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SecondPuzzle
+{
+    public class PuzzleSolver
+    {
+
+        private readonly List<List<int>> _input = new List<List<int>>();
+
+        public PuzzleSolver(IEnumerable<string> input)
+        {
+            foreach (var line in input)
+            {
+                _input.Add(Regex.Split(line, @"[\s\t]+").Select(c => Int32.Parse(c.ToString())).OrderByDescending(i => i).ToList());
+            }
+        }
+
+        public int SolveFirst()
+        {
+            var sum = 0;
+            foreach (var row in _input)
+            {
+                sum += row.First() - row.Last();
+            }
+            return sum;
+        }
+
+        public int SolveSecond()
+        {
+            var sum = 0;
+            foreach (var row in _input)
+            {
+                sum += SumOfDivisible(row);
+            }
+            return sum;
+        }
+
+        private static int SumOfDivisible(IReadOnlyList<int> row)
+        {
+            for (int i = row.Count - 1; i >= 0; i--)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (row[j] % row[i] == 0)
+                        return row[j] / row[i];
+                }
+            }
+            return 0;
+        }
+    }
+}
